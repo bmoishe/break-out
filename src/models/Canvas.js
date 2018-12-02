8693d3032f9ed34224d3e38d5f6f5580d5ee24f2
bmoishe <moishebarnes@gmail.com>
2018-12-02 11:38:10 +0000

    Breakout game

---
+++ src/models/Canvas.js
@@ -0,0 +1,185 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+// defining x and y axis
+var x = canvas.width/2;
+var y = canvas.height-30;
+// add a small value to x and y after every frame has been drawn to make it appear that the ball is moving
+var dx = 2;
+var dy = -2;
+
+var ballRadius = 10;
+
+// Paddle variables
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+// Pressed buttons can be defined and initialized with boolean variables. The default value for both is false because at the beginning the control buttons are not pressed.
+var rightPressed = false;
+var leftPressed = false;
+// Brick variables
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+// 2d array for bricks
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+
+// score counter
+var score = 0;
+
+// Listeners for key presses
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function collisionDetection() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if(score == brickRowCount*brickColumnCount) {
+                        alert("You won. Now build something original");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
+
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+}
+
+function keyDownHandler(e) {
+  // key code 37 is the left cursor key and 39 is the right cursor.
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            if(bricks[c][r].status == 1) {
+                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+
+// The draw() function will be executed within setInterval every 10 miliseconds:
+function draw() {
+  // clearRect() this clears canvas content
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // This paints a new position each frame
+  drawBricks();
+  drawBall();
+  collisionDetection();
+  drawPaddle();
+  drawScore();
+  // This ensures if the ball hits the sides then it will reverse the direction.
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if(y + dy < ballRadius) {
+    dy = -dy;
+  } else if(y + dy > canvas.height-ballRadius) {
+    if(x > paddleX && x < paddleX + paddleWidth) {
+        dy = -dy;
+    }
+    else {
+        alert("GAME OVER");
+        document.location.reload();
+    }
+  }
+// Moves the paddle within the canvas
+  if(rightPressed && paddleX < canvas.width-paddleWidth) {
+      paddleX += 7;
+    }
+  else if(leftPressed && paddleX > 0) {
+      paddleX -= 7;
+    }
+    x += dx;
+    y += dy;
+}
+
+
+setInterval(draw, 10);
+
+
+// red rectangle
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle = "#FF0000";
+// ctx.fill();
+// ctx.closePath();
+//
+// // green circle
+// ctx.beginPath();
+// ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+// ctx.fillStyle = "green";
+// ctx.fill();
+// ctx.closePath();
+//
+// ctx.beginPath();
+// ctx.rect(160, 10, 100, 40);
+// ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+// //  stroke() to only colour the outer stroke
+// ctx.stroke();
+// ctx.closePath();
